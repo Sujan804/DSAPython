0db4cfc2a20181ab12b3839c39dc440ef70d7573
Sujan804 <sujanmec804@gmail.com>
2022-12-20 16:39:43 +0600

    20Dec2022 Blind75

---
+++ Array & Hashing/121. Best Time to Buy and Sell Stock.py
@@ -0,0 +1,29 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        length = len(prices)
+        minArr = [0]*length
+        maxArr = [0]*length
+        curMin = prices[0]
+       
+        minArr[0] = curMin
+        for i in range(1,length): 
+            if prices[i]<curMin:
+                curMin = prices[i]
+            minArr[i] = curMin
+        
+        curMax = prices[length-1]
+
+        maxArr[length-2] = curMax
+        for i in range(length-1,-1,-1):
+            if prices[i] > curMax:
+                curMax = prices[i]
+            maxArr[i] = curMax
+
+        maxProfit = 0
+        for i in range(0, length):
+            if maxProfit < (maxArr[i]-minArr[i]):
+                maxProfit = maxArr[i]- minArr[i]
+
+        return maxProfit
+
+
