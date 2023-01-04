0213b5775271434806c6e416f60980386f0d33fe
Sujan804 <sujanmec804@gmail.com>
2023-01-04 14:17:10 +0600

    04Jan2023 DailyChallenge

---
+++ Daily Challenges 2023/January/2244. Minimum Rounds to Complete All Tasks.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minimumRounds(self, tasks: List[int]) -> int:
+        countFreq = Counter(tasks)
+        res = 0
+        for element in countFreq:
+            if countFreq[element] == 1:
+                return -1
+            res += int((countFreq[element]+2)/3)
+        return res
+            
+        
