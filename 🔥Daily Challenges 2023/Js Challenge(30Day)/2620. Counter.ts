947f1c879aaec223587e538bcd4bc3876f3ec166
Sujan804 <sujanmec804@gmail.com>
2023-05-06 10:32:59 +0600

    day2 closure

---
+++ 🔥Daily Challenges 2023/Js Challenge(30Day)/2620. Counter.ts
@@ -0,0 +1,14 @@
+function createCounter(n: number): () => number {
+  let currentCount = n - 1;
+  return function () {
+    currentCount += 1;
+    return currentCount;
+  };
+}
+
+/**
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
