bb55f63cad0d324223018b943f9530cfe66d802c
Sujan804 <sujanmec804@gmail.com>
2023-05-09 01:32:45 +0600

    >1h s

---
+++ Math & Geometry 📐/1041. Robot Bounded In Circle.py
@@ -0,0 +1,17 @@
+from collections import deque
+
+
+class Solution:
+    def isRobotBounded(self, instructions: str) -> bool:
+        dirX, dirY = 0, 1
+        x, y = 0, 0
+
+        for d in instructions:
+          if d == 'G':
+            x, y = x + dirX, y + dirY
+          elif d == 'R':
+            dirX, dirY = -1*dirY, dirX
+          else:
+            dirX, dirY = dirY, -1*dirX
+
+        return (x,y) == (0,0) or (dirX,dirY)!=(0,1)
